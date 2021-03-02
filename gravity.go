9c88d80eae1c81fa2b3d7bfa334155769bcc225a
DESKTOP-MQS8GBN\hr7te <hriteshbebop@gmail.com>
2021-03-03 00:10:57 +0530

    println commit

--- gravity.go
+++ gravity.go
@@ -5,7 +5,11 @@ import "fmt"
 func main() {
 
 	const earthsGravity = 9.80665
-
 	fmt.Println(earthsGravity)
+
+	fmt.Println("  __      _")
+	fmt.Println("o'')}____//")
+	fmt.Println(" `_/      )")
+	fmt.Println(" (_(_/-(_/")
 
 }
