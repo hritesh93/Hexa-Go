1082301692f038f0d1d66b0c39067ac8b7218ddc
DESKTOP-MQS8GBN\hr7te <hriteshbebop@gmail.com>
2021-03-05 19:39:06 +0530

    dereference commit

--- dereferencing.go
+++ dereferencing.go
@@ -4,7 +4,10 @@ import "fmt"
 
 func main() {
 	star := "Polaris"
-	
+
 	starAddress := &star
-  fmt.Printf("The address of star is %v",starAddress)
+
+	*starAddress = "Sirius"
+
+	fmt.Println("The actual value of star is", star)
 }
